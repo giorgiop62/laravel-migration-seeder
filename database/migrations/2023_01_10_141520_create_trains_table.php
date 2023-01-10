bf974bd8c94bb8261da8047eed0ecccbc2b2479b
Giorgio Perez <giorgiop62@gmail.com>
2023-01-10 15:57:11 +0100

    first commit

---
+++ database/migrations/2023_01_10_141520_create_trains_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+     //Azienda
+     //Stazione di partenza
+     //Stazione di arrivo
+     //Orario di partenza
+     //Orario di arrivo
+     //Codice Treno
+     //Numero Carrozze
+     //In orario
+     //Cancellato
+
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_azienda', 255);
+            $table->string('name_station_p',255);
+            $table->string('name_station_a',255);
+            $table->string('orario_partenza',);
+            $table->string('orario_arrivo',255);
+            $table->string('codice_treno',255);
+            $table->integer('num_carrozze');
+            $table->boolean('treno_in_orario');
+            $table->boolean('cancellato');
+
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
